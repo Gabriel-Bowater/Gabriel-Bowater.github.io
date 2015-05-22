cdfea34a92c7d3ca7a023eb77649245200f91436
Gabriel-Bowater <gd.bowater@gmail.com>
2015-05-22 15:50:40 +1200

    Added lightbox to the gallery page for js enabled browsers

--- js/script.js
+++ js/script.js
@@ -3,3 +3,48 @@ $(document).ready(function(){
 	$('.cuteApple').css('scale', '1.2', 5000);
 	console.log('document ready');
 });
+
+//problem - clicking on images leads to dead end
+//solution - create an overlay with the fullsized image; a lightbox.
+
+var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
+var $caption = $('<p></p>');
+// add image
+$overlay.append($image);
+// add caption
+$overlay.append($caption);
+
+
+//add overlay
+
+
+
+$('body').append($overlay);
+
+
+// get clicks on the image links
+$('#gallery a').click(function(event){
+	event.preventDefault();
+	
+	// update overlay with linked image
+	var imageLocation = $(this).attr('href');
+	$image.attr('src', imageLocation);
+	//debugging
+	console.log(imageLocation);
+
+	//display image's alt text as caption
+	var captionText = $(this).children('img').attr('alt');
+	
+	$caption.text(captionText);
+	//show overlay
+
+
+	$overlay.show();
+});
+
+
+//an escape condition (click to close)
+$overlay.click(function(){
+	$overlay.hide();
+});
